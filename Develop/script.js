688a24f059b469e60b475bd5dab1d3d829352b99
Joseph Nielsen <josephmnielsen@gmail.com>
2021-02-11 19:37:55 -0800

    first push- working generator

---
+++ Develop/script.js
@@ -0,0 +1,38 @@
+
+document.getElementById('generate').addEventListener('click', () => {
+ 
+  let userLength = prompt('How many characters would you like your password to be? (8 min-128 max)')
+  let hasLowercase = prompt('Would you like to use lowercase letters? (y or n)')
+  let hasUppercase = prompt('Would you like to use uppercase letters? (y or n)')
+  let hasNumbers = prompt('Would you like to use numbers? (y or n)')
+  let hasSymbols = prompt('would you like to use symbols? (y or n)')
+  let charSet = ''
+  let lowercase = 'abcdefghijklmnopqrstuvwxyz'
+  let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let numbers = '0123456789'
+  let symbols = '!@#$%&*'
+  let generatedUsername = ''
+
+  if (hasLowercase === 'y') {
+    charSet += lowercase
+  }
+
+  if (hasUppercase === 'y') {
+    charSet += uppercase
+  }
+
+  if (hasNumbers === 'y') {
+    charSet += numbers
+  }
+
+  if (hasSymbols === 'y') {
+    charSet += symbols
+  }
+
+  for (let i = 0; i < userLength; i++) {
+    generatedUsername += charSet[Math.floor(Math.random() * charSet.length)]
+  }
+
+  document.getElementById('password').textContent = generatedUsername
+  
+})
